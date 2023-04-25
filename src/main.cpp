8b9aaf77f415804377808eed6965646e00171497
thiccoder <dom250506@gmail.com>
2023-04-25 16:14:52 +0300

    smooth turns

--- src/main.cpp
+++ src/main.cpp
@@ -5,83 +5,28 @@
 DFRobot_QMC5883 compass(&Wire, HMC5883L_ADDRESS);
 static const uint32_t GPS_BAUD = 9600;
 TinyGPSPlus gps;
-double laat[10]={},lnng[10]={};
-String buf = "";
-bool ready =false;
-int now=0;
-int n=0;
-float heading=0;
-
-void forward(int power)
+double laat[2] = {0, 50.595409}, lnng[2] = {0, 36.587220};
+int current = 0;
+int count = 2;
+void moveTank(int power, int dir)
 {
+  Serial.print("Move with power ");
+  Serial.print(power);
+  Serial.print("and dir ");
+  Serial.println(dir);
 }
-void backward(int power)
-{
-}
-void right(int power)
-{
-}
-void left(int power)
-{
-}
-
 void setup()
 {
   Serial.begin(115200);
   Serial1.begin(GPS_BAUD);
   while (!compass.begin())
   {
-    Serial.println("Could not find a valid 5883 sensor, check wiring!");
+    Serial.println("Could not find compass!");
     delay(500);
   }
-  Serial.println("Initialize HMC5883");
-  // compass.setRange(HMC5883L_RANGE_1_3GA);
-  Serial.print("compass range is:");
-  Serial.println(compass.getRange());
-  // compass.setMeasurementMode(HMC5883L_CONTINOUS);
-  Serial.print("compass measurement mode is:");
-  Serial.println(compass.getMeasurementMode());
-  // compass.setDataRate(HMC5883L_DATARATE_15HZ);
-  Serial.print("compass data rate is:");
-  Serial.println(compass.getDataRate());
-  // compass.setSamples(HMC5883L_SAMPLES_8);
-  Serial.print("compass samples is:");
-  Serial.println(compass.getSamples()); 
+  Serial.println("Initialize compass");
   float declinationAngle = (9.0 + (28.0 / 60.0)) / (180 / PI);
   compass.setDeclinationAngle(declinationAngle);
-  while (true) if (Serial.available())
-  {
-    char c = Serial.read();
-    if ((c == '\n') && (buf != "")) 
-    {
-      break;
-    }
-    else 
-    {
-      buf.concat(c);
-    }
-  }
-  String s = ""; 
-  for (int i = 1;buf[i] != '$';i++)
-  {
-    s.concat(buf[i]);
-  }
-  n = s.toInt();
-  int idx = s.length() + 2;
-  for (int i = 0; i<n;i++)
-  {
-    s = "";
-    while (buf[idx] != '$') s.concat(buf[idx++]);
-    s.replace(',','.');
-    laat[i] = s.substring(0,s.indexOf('&')).toDouble();
-    lnng[i] = s.substring(s.indexOf('&')+1).toDouble();
-    Serial.print("point(lat = ");
-    Serial.print(laat[i], 16);
-    Serial.print(",      lng = ");
-    Serial.print(lnng[i], 16);
-    Serial.println(")");
-    idx++;
-  }
 }
 
 void loop()
@@ -95,40 +40,114 @@ void loop()
         delay(10);
         sVector_t mag = compass.readRaw();
         compass.getHeadingDegrees();
-        heading = mag.HeadingDegress;
-        double course = gps.courseTo(gps.location.lat(),gps.location.lng(),laat[now],lnng[now]);
-        if(course+20<heading)
+        double heading = mag.HeadingDegress;
+        double course = gps.courseTo(gps.location.lat(), gps.location.lng(), laat[current], lnng[current]);
+        double dir = heading - course;
+        double distance = gps.distanceBetween(gps.location.lat(), gps.location.lng(), laat[current], lnng[current]);
+        if (distance > 20)
         {
-          left(90);
-        }
-            else if(course-20>heading)
-        {
-          right(90);
+          moveTank(200, dir);
         }
         else
         {
-          forward(0);
-          if (gps.distanceBetween(gps.location.lat(),gps.location.lng(),laat[now],lnng[now])>17)
+          moveTank(0, 0);
+          delay(1000);
+          Serial.print(laat[current], 8);
+          Serial.print(";");
+          Serial.println(lnng[current], 8);
+          delay(1000);
+          current++;
+          if (count == current)
           {
-            forward(180);
-          }
-          else
-          {
-            forward(0);
-            delay(1000);
-            Serial.print(laat[now], 8);
-            Serial.print(";");
-            Serial.println(lnng[now], 8);
-            delay(1000);
-            now++;
-            if(n==now)
-            {
-              forward(0);
-              delay(999999);
-            }
+            moveTank(0, 0);
+            delay(999999);
           }
         }
       }
     }
   }
 }
+/*
+#include <TinyGPSPlus.h>
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+void displayInfo();
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPSPlus with an attached GPS module"));
+  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial1.available() > 0)
+    if (gps.encode(Serial1.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID: "));
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}*/
